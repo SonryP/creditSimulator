6e4a840f6298195db6ceb715fce3a7fcb615271b
Cristopher Herrera <c.herreratapia@hotmail.com>
2018-11-05 00:53:19 -0300

    JS
    Deleted: Datepicker and Selectize
    Added: flatpickr, Select2 and date validate

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,11 +1,7 @@
 
 
-document.addEventListener("DOMContentLoaded", function() {
+$(document).ready(function() {
   
-    // Init selectize and datepicker
-    $('[data-toggle="datepicker"]').datepicker();
-    $('#select-months').selectize();
-
     // Variables of ranges
     var sliderAmount = $('#sliderAmount');
     var sliderFees = $('#sliderFees');
@@ -13,12 +9,38 @@ document.addEventListener("DOMContentLoaded", function() {
     // Span of amount of ranges
     var amount = $('#simulatorAmount');
     var fees = $('#simulatorFees');
+
+    // Variables to Check
+    var amountValue = 100;
+    var feesValue = 4;
+    var dateSelected = new Date();
     
     // Inputs
     var rut = $('#rutInput');
     var salary = $('#salaryInput')
     var date = $('#datePickerInput');
     var selectmonths = $('#select-months');
+    
+    // Init multiple select 
+    selectmonths.select2({
+        placeholder: 'Seleccione mes...',
+        maximumSelectionLength: 2,
+        'language': 'es',
+    });
+
+    // Init datepicker
+    date.flatpickr({
+        dateFormat: "d-m-Y",
+        minDate: new Date().fp_incr(21),
+        maxDate: new Date().fp_incr(30),
+        locale: es.Spanish,
+        onChange: function(selectedDates, dateStr, instance){
+            dateSelected = new Date(selectedDates[0]);
+            selectmonths.prop('disabled', false);
+        }
+    });
+    
+    
 
     // Submit
     var btnSubmit = $('#submitSimulator');
@@ -33,6 +55,7 @@ document.addEventListener("DOMContentLoaded", function() {
     // Toggle disabled of inputs
     rut.change(function() {
         var datos;
+        
         if (rut.value === '') {
             salary.prop("disabled", true);
         } else {
@@ -46,6 +69,7 @@ document.addEventListener("DOMContentLoaded", function() {
                 success: function (data) {
                     data= $.parseJSON(data);
                     if (data.code === "OK"){
+                        rut.val(rutFormat(rut.val()));
                         salary.prop("disabled", false);
                     }else{
                         alert("El rut no es valido, ingrese nuevamente.");
@@ -78,6 +102,7 @@ document.addEventListener("DOMContentLoaded", function() {
                 success: function (data) {
                     data= $.parseJSON(data);
                     if (data.code === "OK"){
+                        salary.val(toMiles(salary.val()));
                         date.prop('disabled', false);
                         sliderFees.prop('disabled', false);
                         sliderAmount.prop('disabled', false);
@@ -89,31 +114,57 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 
-    // Replace text of span from range
-    sliderAmount.change(function() {
-        console.log(sliderAmount.val());
-        amount.text( `\$${sliderAmount.val()}`);
+    // sliderAmount events
+    sliderAmount.on('input', function(){
+        amount.text(`$${toMiles($(this).val())}`);
+    });
+
+    sliderAmount.on('change', function(){
+        amountValue = $(this).val();
+        console.log(amountValue);
     });
     
-    sliderFees.change(function() {
-        console.log(sliderFees.val());
-        console.log(fees.html())
-        fees.text(`Cuotas: ${sliderFees.val()}`);
+    // sliderFees events 
+    sliderFees.on('input', function(){
+        fees.text(`Cuotas: ${$(this).val()}`);
     });
 
+    sliderFees.on('change', function(){
+        feesValue = $(this).val();
+        console.log(feesValue);
+    });
+    
+    // Add point to miles
+    function toMiles(number) {
+        return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+    }
 
+    // Rut formatter 
+    function rutFormat(rut) {
+        var rutWO = rut.replace(/\.-/g, '');
+        var dv = rutWO[(rutWO.length - 1)];
+        var rut = rutWO.substring(0, (rutWO.length - 1));
+        rut = toMiles(rut);
+        return (rut + '-' + dv);
+    }
 
+    // toValidate
     btnSubmit.click(function(){
         var validate = true;
 
+        if ((amountValue >= 100 && amountValue <= 100000000) && (feesValue >= 4 && feesValue <= 60)) {
+            validate = true;
+        } else {
+            validate = false;
+        }
         if (salary.val() === '' || rut.val() === '' || amount.val() === '' || fees.val() === '' || date.val() === '') {
             validate = false;
         }
 
         if (validate) {
-            alert('DALE');
+            // TODO SUBMIT
         } else {
-            alert("NEGATIVO");
+            // TODO NO SUBMIT
         }
     });
 
